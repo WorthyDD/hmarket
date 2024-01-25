3591deb885da57fe9aab44641b9d54a752f0a3e6
Woods <woods@bipal.com>
2024-01-25 14:25:51 +0700

    2024

--- pyschripts/main.py
+++ pyschripts/main.py
@@ -1,19 +1,17 @@
 #coding：utf-8
-import traceback
-import random
 import re
-# import requests
+import traceback
 import os
 import shutil
 import platform
 import json
-import time
 import pandas as pd
 
 
 #配置
+# city = 'chengdu'
 city = 'xian'
-date = '2023-11-23'
+date = '20240125'
 
 def read_download_data():
 
@@ -23,7 +21,7 @@ def read_download_data():
     print(path)
     # path = os.curdir+"/data/"
     # print(path)
-    cur_img_path = f".\\data\\{city}" if type == "Windows" else f"./data/{city}/"
+    cur_img_path = f".\\data\\{city}" if type == "Windows" else f"./data/{city}/{date}"
     for root,folders,files in os.walk(path):
         for file in files:
             # print(file)
@@ -42,10 +40,16 @@ def read_download_data():
                 continue
 
 
+def clean_sheet_name(name):
+    # 移除非法字符
+    cleaned_name = re.sub(r'[\\/*?:\[\]]', '', name)
+    return cleaned_name[:31]  # 限制长度为31个字符，以确保符合 Excel 的限制
+
+
 def writeToExcel():
 
-    datadir = f'./data/{city}'
-    target_excel = f'./data/{city}/lianjia.xlsx'
+    datadir = f'./data/{city}/{date}'
+    target_excel = f'./data/{city}/{date}/lianjia.xlsx'
 
 
     files = os.listdir(datadir)
@@ -70,15 +74,56 @@ def writeToExcel():
 
             except Exception as e:
                 print(filename, '----------', e)
+    # df['title'] = df['title'].apply(clean_sheet_name)
+    mask = df['title'].str.contains('A级景区房，毛坯房任意发挥装修')
+    df.loc[mask, 'title'] = '未知标题'
 
     if not os.path.exists(target_excel):
         df.to_excel(target_excel, index=False, sheet_name=date)
     else:
         # 追加到现有的 Excel 文件
         with pd.ExcelWriter(target_excel, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-            df.to_excel(writer, index=False, sheet_name=date, header=False)
+            df.to_excel(writer, index=False, sheet_name=date, header=True)
+
+def mergeExcel():
+    # datadir = f'./data/{city}/{date}'
+    target_excel = f'./data/{city}/{date}/lianjia.xlsx'
+    sheet_name1 = '20231222'
+    sheet_name2 = '20240125'
+    df1 = pd.read_excel( target_excel, sheet_name=sheet_name1)
+    df2 = pd.read_excel(target_excel, sheet_name=sheet_name2)
+    df2 = df2.drop(columns=['title', 'desc', 'follow'])
+    df1.rename(columns={'price':'price1'}, inplace=True)
+    df2.rename(columns={'price':'price2'}, inplace=True)
+
+    print(df1.columns.tolist(), df2.columns.tolist())
+
+    df1['price1'] = df1['price1'].str.extract(r'([0-9.]+)')
+    df1['price1'] = pd.to_numeric(df1['price1'], errors='coerce')
+
+    df2['price2'] = df2['price2'].str.extract(r'([0-9.]+)')
+    df2['price2'] = pd.to_numeric(df2['price2'], errors='coerce')
+
+
+    print(df1.columns.tolist(), df2.columns.tolist())
+    merged_df = pd.merge(df1, df2, on='link',how='inner')
+    print(merged_df)
+    merged_df['price_diff'] = merged_df['price1'] - merged_df['price2']
+    merged_df['percent'] = (merged_df['price_diff'] / merged_df['price1'])
+
+    
+
+    print(merged_df.columns.tolist())
+    with pd.ExcelWriter(target_excel, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+            merged_df.to_excel(writer, index=False, sheet_name='202401merged', header=True)
+
+
+
+
 
 if __name__ == '__main__':
     # read_download_data()
 
     writeToExcel()
+
+    mergeExcel()
